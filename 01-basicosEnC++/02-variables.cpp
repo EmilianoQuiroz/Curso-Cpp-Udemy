13ec21f56956f7664975d0d5d909789ac3268a7c
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-11 12:01:03 -0300

    Variables en C

---
+++ 01-basicosEnC++/02-variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //Variables en C++
+    /*Una variable es un contenedor para un valor que nosotros especifiquemos, este valor puede ir cambiando a lo largo del programa*/
+    string nombre = "Pepe";
+    int edad = 24;
+    int edad_siguiente = edad +1;
+
+    cout<< "Me llamo "<< nombre << " y tengo "<< edad << " años."<<" El año que viene cumplo "<<edad_siguiente<<endl;
+
+    return 0;
+}
