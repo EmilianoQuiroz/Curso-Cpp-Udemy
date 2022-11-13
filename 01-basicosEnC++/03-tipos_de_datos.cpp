baa84c4306e824599e3fd1b510955892fa77db5e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-13 00:41:41 -0300

    Tipos de datos en C

---
+++ 01-basicosEnC++/03-tipos_de_datos.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //Tipos de datos en C++
+    //Datos de tipo texto
+    //Tipo de dato caracter
+    char letra = 'A';
+    //Tipo de dato string
+    string frase = "Tipo de dato string";
+    //Tipo de dato numero entero
+    int edad = 24;
+    //Tipo de dato numero flotante
+    double nota = 7.8;
+    //Valores booleanos
+    bool aprobado = true;
+
+    //Mostramos los datos por pantalla
+    cout << frase << endl;
+    
+    return 0;
+}
