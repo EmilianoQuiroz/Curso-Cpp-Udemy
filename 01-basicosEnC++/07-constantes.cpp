c61317bb0a7772ce041e840994c7b144e2a40676
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-13 18:58:30 -0300

    Constantes en C++

---
+++ 01-basicosEnC++/07-constantes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //Constantes en C++
+    /*Las constantes son contedores de informacion que no cambian su valor inicial*/
+    const string CONSTANTE = "Esta constante no puede cambiar su valor";
+    /*Primero las definimos con la paabra reservada const luego el tipo de dato que ira en ella y por ultimo el nombre que le asignaremos a la misma, se recomienda siempre nombrarlas en mayuscula como una buena practica*/
+
+    cout<<CONSTANTE<<endl;
+
+    return 0;
+}
